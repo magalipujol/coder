b68964c3abc19d4b0f9a3726414b1fd68af19f64
magali <magalipuuujol@gmail.com>
2022-08-25 16:00:39 -0300

    index.js terminado

--- Desafios/entrega-5/source/index.js
+++ Desafios/entrega-5/source/index.js
@@ -6,3 +6,47 @@
 Hacer lo mismo utilizando los tres motores de plantllas (handlebars, pug y ejs)
 */
 
+const express = require('express');
+// TODO chequear por quee hay dos, y que ruta va
+const { router, productos } = require('./routes');
+
+const { engine } = require('express-handlebars')
+const ejs = require('ejs');
+const pug = require('pug');
+
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, (req, res) => {
+    console.log(`Server running on port ${PORT}`);
+})
+
+// configuración para el motor de plantillas handlebars
+app.engine('.hbs', engine({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
+
+
+// configuración para el motor de plantillas pug
+// app.set('view engine', 'pug')
+
+// configuración para el motor de plantillas ejs
+// app.set('view engine', 'ejs')
+
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('api/productos', router)
+
+app.get('/', (req, res) => {
+    res.render('form')
+})
+
+app.get('/productos', (req, res) => {
+    res.render('productos', { array: productos })
+})
+
