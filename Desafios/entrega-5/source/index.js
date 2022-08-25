67a97c38c2adf2fa1aec846c8353d449381d7c1c
magali <magalipuuujol@gmail.com>
2022-08-25 15:52:19 -0300

    inicia el proyecto

---
+++ Desafios/entrega-5/source/index.js
@@ -0,0 +1,8 @@
+/* Utilizando la misma API de productos del proyecto anterior, construir un web server (no REST) que tenga:
+    a) un formulario de carga de productos en la ruta raíz (configurar la ruta /productos para recibir el POST y redirigir al mismo formulario)
+    b) una vista de productos cargados en la ruta GET /productos
+    c) ambas páginas contarán con un botón que redirija a la otra
+
+Hacer lo mismo utilizando los tres motores de plantllas (handlebars, pug y ejs)
+*/
+
