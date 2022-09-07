ae821864f0122a21574cb2ce54dc48e5e4ff7a9e
magali <magalipuuujol@gmail.com>
2022-09-06 21:10:24 -0300

    avances (todavía no funciona)

--- Desafios/entrega-6/source/index.js
+++ Desafios/entrega-6/source/index.js
@@ -1,78 +1,53 @@
-/*
-Consigna 1: Modificar el último entregable para que disponga de un canal de websocket que permita representar, por debajo del formulario de ingreso, una tabla con la lista de productos en tiempo real.
-
-    Puede haber varios clientes conectados simultáneamente y en cada uno de ellos se reflejarán los cambios que se realicen en los productos sin necesidad de recargar la vista.
-    Cuando un cliente se conecte, recibirá la lista de productos a representar en la vista.
-
-*/
-const express = require('express');
-const { router, productos } = require('./api.js');
-const { engine } = require('express-handlebars');
-const path = require('path');
+const express = require("express");
+const { router, productos } = require("./api.js");
+const { engine } = require("express-handlebars");
+const path = require("path");
 const { Server: HttpServer } = require("http");
 const { Server: IOServer } = require("socket.io");
 
-const httpServer = new HttpServer(app)
-const io = new IOServer(httpServer);
 const app = express();
+const httpServer = new HttpServer(app);
+const io = new IOServer(httpServer);
 const PORT = process.env.PORT || 3000;
 
 const connectedServer = app.listen(PORT, (req, res) => {
-    console.log(`Server running on port ${PORT}`);
-})
+  console.log(`Server running on port ${PORT}`);
+});
 
-app.engine('.hbs', engine({ extname: '.hbs' }))
+app.engine(".hbs", engine({ extname: ".hbs" }));
 
-app.set('view engine', '.hbs')
-app.set('views', path.join(__dirname, 'views'))
+app.set("view engine", ".hbs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, 'public')))
-app.use('/', router)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/", router);
 
-app.get('/', (req, res) => {
-    res.render('table', { productos: productos })
-})
+app.get("/", (req, res) => {
+  res.render("table", { productos: productos });
+});
 
-app.get('/form', (req, res) => {
-    res.render('form', { productos: productos })
-})
+app.get("/form", (req, res) => {
+  res.render("form", { productos: productos });
+});
+
+
+// borrar esta variable
+let products = [
+    {
+      nombre: "labradorcito",
+      precio: "1000 mimos x día",
+      thumbnail: "https://thumbs.dreamstime.com/b/sentada-del-perrito-de-labrador-30817211.jpg",
+      id: 1,
+    },
+  ];
 
 io.on("connection", async (socket) => {
-    console.log('new user connected');
+  console.log("new user connected");
 
-    socket.emit('productos', productos);
+  socket.emit("productos", productos);
 
-    // acá debería usar la función de GuardarProducto de main.js
-    socket.on('new-product', async (data) => {
-        await guardarProducto(data);
-        io.sockets.emit('productos', productos);
-    })
-})
+});
 
-connectedServer.on("error", error => console.log(`error: ${error}`))
-
-// funciones auxiliares
-
-const guardarProducto = async () => {
-    console.log("se llamó a guardarProducto desde index.js");
-    const nombre = document.getElementById('nombre').value
-    const precio = document.getElementById('precio').value
-    const thumbnail = document.getElementById('thumbnail').value
-
-    try {
-        const data = { nombre, precio, thumbnail}
-        const response = await fetch('/productos', {
-            method: 'POST',
-            body: JSON.stringify(data),
-            headers: { 'Content-Type': 'application/json'},
-        })
-        const result = await response.json()
-        return result
-
-    } catch (error) {
-        let err = new Error(error)
-        return err
-    }
-}
+connectedServer.on("error", (error) => console.log(`error: ${error}`));
--- Desafios/entrega-6/source/public/main.js
+++ Desafios/entrega-6/source/public/main.js
@@ -1,22 +1,41 @@
-const guardarProducto = async () => {
-    const nombre = document.getElementById('nombre').value
-    const precio = document.getElementById('precio').value
-    const thumbnail = document.getElementById('thumbnail').value
+// const guardarProducto = async () => {
+//     const nombre = document.getElementById('nombre').value
+//     const precio = document.getElementById('precio').value
+//     const thumbnail = document.getElementById('thumbnail').value
 
-    try {
-        const data = { nombre, precio, thumbnail}
-        const response = await fetch('/productos', {
-            method: 'POST',
-            body: JSON.stringify(data),
-            headers: { 'Content-Type': 'application/json'},
-        })
-        const result = await response.json()
-        return result
+//     try {
+//         const data = { nombre, precio, thumbnail}
+//         const response = await fetch('/productos', {
+//             method: 'POST',
+//             body: JSON.stringify(data),
+//             headers: { 'Content-Type': 'application/json'},
+//         })
+//         const result = await response.json()
+//         return result
 
-    } catch (error) {
-        let err = new Error(error)
-        return err
-    }
+//     } catch (error) {
+//         let err = new Error(error)
+//         return err
+//     }
+// }
+
+const socket = io.connect();
+
+function obtenerTemplateProductos(productos) {
+    return fetch('templates/productos-tabla.hbs').then((response) => {
+        response.text
+    })
+    .then((template) => {
+        const templateCompiled = Handlebars.compile(template)
+        return templateCompiled({ productos })
+    });
 }
+
+
+
+socket.on("productos", async productos => {
+    const html = await obtenerTemplateProductos(productos);
+    document.getElementById('products').innerHTML = html;
+})
 
 console.log("script funcionando");
