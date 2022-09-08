99d792053e47020801c817bd293bce8c124ae36f
magali <magalipuuujol@gmail.com>
2022-09-08 20:29:23 -0300

    arregla error

--- Desafios/entrega-6/source/public/main.js
+++ Desafios/entrega-6/source/public/main.js
@@ -7,40 +7,39 @@ const guardarProducto = async () => {
     const precio = document.getElementById('precio').value
     const thumbnail = document.getElementById('thumbnail').value
 
-    socket.emit('nuevo-producto', { nombre, precio, thumbnail })
     try {
-        const data = { nombre, precio, thumbnail}
-        const response = await fetch('/productos', {
-            method: 'POST',
-            body: JSON.stringify(data),
-            headers: { 'Content-Type': 'application/json'},
-          })
-          // emit to all clients
-        const result = await response.json()
-        return result
+      const data = { nombre, precio, thumbnail}
+      const response = await fetch('/productos', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: { 'Content-Type': 'application/json'},
+      })
+      // emit to all clients
+      const result = await response.json()
+      return result
 
     } catch (error) {
-        let err = new Error(error)
-        return err
+      let err = new Error(error)
     }
+    socket.emit('nuevo-producto', { nombre, precio, thumbnail })
 }
 
 // socket para tabla de productos
 // descomentar para que funcione
-function obtenerTemplateChat(productos) {
-  return fetch("templates/productos-tabla.hbs")
-    .then((response) => response.text())
-    .then((template) => {
-      const templateCompiled = Handlebars.compile(template);
-      return templateCompiled({ productos });
-    });
-}
+// function obtenerTemplateChat(productos) {
+//   return fetch("templates/productos-tabla.hbs")
+//     .then((response) => response.text())
+//     .then((template) => {
+//       const templateCompiled = Handlebars.compile(template);
+//       return templateCompiled({ productos });
+//     });
+// }
 
-socket.on("productos", async (productos) => {
-    console.log('llegaron products');
-  const html = await obtenerTemplateChat(productos);
-  document.getElementById("products").innerHTML = html;
-});
+// socket.on("productos", async (productos) => {
+//     console.log('llegaron products');
+//   const html = await obtenerTemplateChat(productos);
+//   document.getElementById("products").innerHTML = html;
+// });
 
 
 // funciones para chat
@@ -50,33 +49,33 @@ const guardarMensaje = async () => {
   const mensaje = document.getElementById('mensaje').value
 
   try {
-      const data = { username, mensaje }
-      const response = await fetch('/mensajes', {
-          method: 'POST',
+    const data = { username, mensaje }
+    const response = await fetch('/mensajes', {
+      method: 'POST',
           body: JSON.stringify(data),
           headers: { 'Content-Type': 'application/json'},
       })
       const result = await response.json()
-      socket.emit('nuevo-mensaje', data)
       return result
 
-  } catch (error) {
+    } catch (error) {
       let err = new Error(error)
-      return err
-  }
+    }
+
+    socket.emit('nuevo-mensaje', data)
 }
 
-// function obtenerTemplateChat(mensajes) {
-//   return fetch("templates/chat.hbs")
-//     .then((response) => response.text())
-//     .then((template) => {
-//       const templateCompiled = Handlebars.compile(template);
-//       return templateCompiled({ mensajes });
-//     });
-// }
+function obtenerTemplateChat(mensajes) {
+  return fetch("templates/chat.hbs")
+    .then((response) => response.text())
+    .then((template) => {
+      const templateCompiled = Handlebars.compile(template);
+      return templateCompiled({ mensajes });
+    });
+}
 
-// socket.on("chat", async (mensajes) => {
-//     console.log('nuevo mensaje');
-//   const html = await obtenerTemplateChat(mensajes);
-//   document.getElementById("complete-chat").innerHTML = html;
-// });
+socket.on("chat", async (mensajes) => {
+    console.log('nuevo mensaje');
+  const html = await obtenerTemplateChat(mensajes);
+  document.getElementById("complete-chat").innerHTML = html;
+});
